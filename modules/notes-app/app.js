1e8742e14a3d9531ea46912f5f6a2a797f0d2da3
phoenixyork166 <berlinlee2017@gmail.com>
2024-02-28 11:26:12 +0800

    28.1 Feb 2024

--- modules/notes-app/app.js
+++ modules/notes-app/app.js
@@ -9,17 +9,33 @@
 // fs.appendFileSync(filePath, ' I live in Philadelphia.')
 // fs.appendFileSync(filePath, '\nThis is a new line ;)')
 
-// import runs first
+// Self-declared modules run first
 // Destructuring variables & callback func from ./utils.js
 const { myName, add } = require('./utils.js')
 // Destructuring the only callback fun in ./notes.js
 const { getNotes } = require('./notes.js')
 
+// 3rd party modules run later
+// importing 3rd party modules
+const validator = require('validator')
+
 const sum = add(1,10)
 console.log(`name: ${myName}`)
 console.log(`\n`)
 console.log(`sum: ${sum}`)
-console.log(`\n`)
 
+console.log(`\n`)
 const msg = getNotes()
 console.log(`msg: ${msg}`)
+
+const email = 'test@test.com';
+console.log(`email: ${email}`)
+
+console.log(`\n`)
+console.log(`validator.isEmail(email):\n${validator.isEmail(email)}`)
+
+console.log(`\n`)
+const myUrl = 'https://google.com.hk'
+
+console.log(`validator.isURL(myUrl):\n${validator.isURL(myUrl)}`);
+
